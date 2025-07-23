cd95349b6b4616af883b4f1d9544ef5c7b964339
saka015 <sknasylum8454@gmail.com>
2025-07-24 02:43:07 +0530

    fix

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['avatar.iran.liara.run'],
   },
+  experimental: {
+    serverComponentsExternalPackages: [],
+  },
 };
 
 export default nextConfig;
